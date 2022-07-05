d8c737eb290687d7e7101d350a5c129d86427b42
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-07-05 20:59:18 +0530

    added dependacies for possible eureka registration

--- src/main/java/com/user/migrate/UsermigrateApplication.java
+++ src/main/java/com/user/migrate/UsermigrateApplication.java
@@ -3,10 +3,11 @@ package com.user.migrate;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+//@EnableEurekaClient
 public class UsermigrateApplication {
 
 	public static void main(String[] args) {
